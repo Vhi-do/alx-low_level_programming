df5df0340646c359c5043e7d1ff0f2eaaf73150d
Vhi-do <olofintiladavid@gmail.com>
2022-03-16 21:06:59 +0100

    nested loops and functions

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -1,8 +1,8 @@
 #include "main.h"
 
 /**
- * jack_bauer - prints every minute of the day of Jack Bauer
- */
+*jack_bauer - prints every minute of the day of Jack Bauer
+*/
 void jack_bauer(void)
 {
 	int i, j;
