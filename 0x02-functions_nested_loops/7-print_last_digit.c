6cdb2f9bd4c5240826bb18ad1c05e3ed02683b85
Vhi-do <olofintiladavid@gmail.com>
2022-03-16 21:05:24 +0100

    nested loops and functions

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -1,9 +1,9 @@
 #include "main.h"
 /**
- * print_last_digit - return last digit
- * @n : number to check
- * Return:0 or 1
- */
+*print_last_digit - return last digit
+*@n : number to check
+*Return:0 or 1
+*/
 
 int print_last_digit(int n)
 {
